9b3f59fd01605fa81e2b3662a080984f54332c3e
Filip Hajdyła <hajdylaf@gmail.com>
2024-03-03 14:34:00 +0100

    fix downstream analysis logging

--- workflow/scripts/downstream_analysis.R
+++ workflow/scripts/downstream_analysis.R
@@ -1,3 +1,11 @@
-library(dplyr)
+# stdout/stderr logging
+log <- file(snakemake@log[[1]], open = "wt")
+sink(log)
 
+# libraries
+suppressMessages({
+  library(dplyr)
+})
+
+# the rest of the script
 file.create(snakemake@output[[1]])

510d7c2c9dd97da4eb701fd350b25269f3d43266
Filip Hajdyła <hajdylaf@gmail.com>
2024-03-03 11:50:13 +0100

    add protein seqeuences filtering/add logs

---
+++ workflow/scripts/filter_protein_sequences.py
@@ -0,0 +1,15 @@
+from Bio import SeqIO
+
+if __name__ == "__main__":
+    with open(snakemake.input[0], "r") as f:
+        records = list(SeqIO.parse(f, "fasta"))
+    filtered = []
+    for r in records:
+        if set(r.seq) <= set("AUGC"):
+            continue
+        elif set(r.seq) <= set("ATGC"):
+            continue
+        else:
+            filtered.append(r)
+    with open(snakemake.output.filtered, "w") as f:
+        SeqIO.write(filtered, f, "fasta")

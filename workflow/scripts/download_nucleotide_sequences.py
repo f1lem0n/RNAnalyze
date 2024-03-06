05db1cfe0654205a1590c95004f19296902f2db4
Filip Hajdyła <hajdylaf@gmail.com>
2024-03-06 15:32:05 +0100

    fix download_nucleotide_sequences

--- workflow/scripts/download_nucleotide_sequences.py
+++ workflow/scripts/download_nucleotide_sequences.py
@@ -1,7 +1,13 @@
-from Bio import Entrez, SeqIO
 from pathlib import Path
 from subprocess import run
 from time import sleep
+
+from Bio import Entrez, SeqIO
+
+from logger import get_logger
+
+
+LOGGER = get_logger(str(snakemake.log))
 
 
 def batch_download(input: Path, output: Path) -> None:
@@ -20,21 +26,28 @@ def batch_download(input: Path, output: Path) -> None:
 def download_sequence(
     accession_number: str, start: int, stop: int, output: Path
 ) -> None:
-    with Entrez.efetch(
-        db="nucleotide",
-        rettype="fasta",
-        retmode="text",
-        id=accession_number,
-    ) as handle:
-        record = SeqIO.read(handle, "fasta")
-        record.seq = record.seq[start - 1 : stop]
-        SeqIO.write(record, output / f"{accession_number}.fasta", "fasta")
+    try:
+        LOGGER.info(
+            f"Downloading {accession_number} ({start}:{stop}) --> {output}"
+        )
+        with Entrez.efetch(
+            db="nucleotide",
+            rettype="fasta",
+            retmode="text",
+            id=accession_number,
+        ) as handle:
+            record = SeqIO.read(handle, "fasta")
+            record.seq = record.seq[start - 1 : stop]
+            SeqIO.write(record, output / f"{accession_number}.fasta", "fasta")
+    except Exception as e:
+        LOGGER.error(f"Error downloading {accession_number}: {e}")
     sleep(1)
 
 
 def combine_sequences(output: Path) -> None:
+    LOGGER.info(f"Combining sequences...")
     run(
-        f"cat {output}/*.fasta > {output}/combined.fasta 2> {snakemake.log} "
+        f"cat {output}/*.fasta > {output}/combined.fasta 2>> {snakemake.log} "
         f"|| touch {output}/combined.fasta",
         shell=True,
     )
